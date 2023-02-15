c4999baa9ab7d9f0ccffd9cc468166f4fb5d9877
ZPythonC <119906572+ZPythonC@users.noreply.github.com>
2023-02-15 12:47:39 -0500

    Add files via upload

---
+++ project/test.py
@@ -0,0 +1,26 @@
+import unittest
+from fractions import Fraction
+
+from my_sum import sum
+
+
+class TestSum(unittest.TestCase):
+    def test_list_int(self):
+        """
+        Test that it can sum a list of integers
+        """
+        data = [1, 2, 3]
+        result = sum(data)
+        self.assertEqual(result, 6)
+    def test_list_fraction(self):
+        """
+        Test that it can sum a list of fractions
+        """
+        data = [Fraction(1, 4), Fraction(1, 4), Fraction(2, 5)]
+        result = sum(data)
+        self.assertEqual(result, 1)
+
+
+if __name__ == '__main__':
+    unittest.main()
+#These test results show that the first test passed while the second test failed, just as indicated in the tutorial. I feel that this is a very effective method for testing code, although the need to look for a specific name on a file might be a handicap.
